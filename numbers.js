c78bb0ccdf3b37de8f9809d7d56b0bff86513216
Daniel Noon <supercookies@outlook.com>
2019-08-12 21:54:51 -0500

    sort and list

--- numbers.js
+++ numbers.js
@@ -1,65 +1,29 @@
-const greisch = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
+const greisch = [3, 0, 0, 0];
+
+const golka = [15, 0, 0, 0];
+
+const spanny = [12, 0, 0, 0];
+
+const delkamiller = [13, 0, 0, 0];
+
+const cotton = [24, 0, 0, 0];
+
+const bauman = [10, 0, 0, 0];
+
+const piernicky = [22, 0, 0, 0];
+
+const sisson = [16, 0, 0, 0];
+
+const mcGill = [5, 0, 0, 0];
+
+export {
+  greisch,
+  golka,
+  spanny,
+  delkamiller,
+  cotton,
+  bauman,
+  piernicky,
+  sisson,
+  mcGill
 };
-
-const golka = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const spanny = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const delkamiller = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const cotton = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const bauman = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const piernicky = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const sisson = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-const mcgill = {
-    w1 : "0",
-    w2 : "0",
-    w3 : "0",
-    w4 : "0",
-};
-
-
-export { greisch , golka , spanny , delkamiller , cotton , bauman , piernicky , sisson , mcgill };
